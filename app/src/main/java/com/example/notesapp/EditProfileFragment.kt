31f23c4a51945fa32d05e6bb9ebb3258589fef0b
Đỗ Xuân Hướng <dohuong0422@gmail.com>
2022-09-20 15:02:01 +0700

    Update

--- app/src/main/java/com/example/notesapp/EditProfileFragment.kt
+++ app/src/main/java/com/example/notesapp/EditProfileFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
@@ -38,12 +39,42 @@ class EditUserFragment : Fragment() {
         val telephone = binding.phoneNumberEDT.text.toString()
         val email = binding.mailEDT.text.toString()
 
-        askActivityToUpdateUser(username, telephone, email)
-        setFragmentResult(Keys.USER_UPDATED, bundleOf())
-        parentFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragmentContent, SettingFragment())
-            commit()
+        if (username.isEmpty()) {
+            Toast.makeText(context, "Your name must not be empty", Toast.LENGTH_SHORT).show()
+        } else if (telephone.isEmpty()) {
+            Toast.makeText(context, "Phone number must not be empty", Toast.LENGTH_SHORT).show()
+        } else if (email.isEmpty()) {
+            Toast.makeText(context, "Email must not be empty", Toast.LENGTH_SHORT).show()
+
+        } else if (!isPhoneNumberValid(telephone)) {
+            Toast.makeText(
+                context,
+                "You did not enter your phone number in the right way!",
+                Toast.LENGTH_LONG
+            ).show()
+        } else if (!isEmailValid(email)) {
+            Toast.makeText(context, "You did not enter email in the right way!", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            Toast.makeText(context, "Saved!", Toast.LENGTH_SHORT).show()
+            askActivityToUpdateUser(username, telephone, email)
+            setFragmentResult(Keys.USER_UPDATED, bundleOf())
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragmentContent, SettingFragment())
+                commit()
+            }
         }
+    }
+
+    private fun isPhoneNumberValid(phoneNumber: String): Boolean {
+        for (i in phoneNumber) {
+            if (i < '0' || i > '9') return false
+        }
+        return true
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
     private fun setUpUserDetail() {
