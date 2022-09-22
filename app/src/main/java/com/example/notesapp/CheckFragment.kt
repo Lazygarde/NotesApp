5f4b6c84a2c20c976be74958261322dc357806ab
Đỗ Xuân Hướng <dohuong0422@gmail.com>
2022-09-22 07:38:41 +0700

    Update

--- app/src/main/java/com/example/notesapp/CheckFragment.kt
+++ app/src/main/java/com/example/notesapp/CheckFragment.kt
@@ -1,13 +1,11 @@
 package com.example.notesapp
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.PopupMenu
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
