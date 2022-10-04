4c816a6f100e572e0bb8a701a3e628e4c56f2503
Đỗ Xuân Hướng <dohuong0422@gmail.com>
2022-10-04 11:16:13 +0700

    Update

--- app/src/main/java/com/example/notesapp/fragment/CalendarFragment.kt
+++ app/src/main/java/com/example/notesapp/fragment/CalendarFragment.kt
@@ -8,9 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.R
 import com.example.notesapp.adapter.CalendarRVAdapter
 import com.example.notesapp.data.NoteViewModel
 import com.example.notesapp.databinding.FragmentCalendarBinding
+import com.github.sundeepk.compactcalendarview.CompactCalendarView
+import com.github.sundeepk.compactcalendarview.domain.Event
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CalendarFragment : Fragment() {
 
@@ -31,6 +36,7 @@ class CalendarFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setUpAdapter()
         setUpCalendar()
+        setupEvents()
     }
 
     private fun setUpAdapter() {
@@ -42,15 +48,61 @@ class CalendarFragment : Fragment() {
     }
 
     private fun setUpCalendar() {
-        binding.calendarView.setOnDateChangeListener { _, year, month, day ->
-            val date = String.format("%02d.%02d.%04d", day, month + 1, year)
-            val listNote = viewModel.calendarSearch(date)
-            listNote.observe(viewLifecycleOwner) { list ->
-                val sortedList = list.sortedBy { allNotes ->
-                    allNotes.time
-                }
-                adapter.updateList(sortedList)
+        val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        val currentDate: String = sdf.format(Date())
+        binding.textMonth.text =
+            SimpleDateFormat("MMMM - yyyy", Locale.getDefault()).format(Date())
+        binding.calendarView.shouldDrawIndicatorsBelowSelectedDays(true)
+        showCalendarItemRecyclerView(currentDate)
+        binding.calendarView.setListener(object : CompactCalendarView.CompactCalendarViewListener {
+            override fun onDayClick(date: Date) {
+                val searchQuery = sdf.format(date)
+                showCalendarItemRecyclerView(searchQuery)
             }
+
+            override fun onMonthScroll(date: Date) {
+                binding.textMonth.text =
+                    SimpleDateFormat("MMMM - yyyy", Locale.getDefault()).format(date)
+            }
+        })
+    }
+
+    private fun setupEvents() {
+        val listNote = viewModel.listNote
+        val set = mutableSetOf<Long>()
+        listNote.observe(viewLifecycleOwner) { notes ->
+            for (note in notes) {
+                val calendar = Calendar.getInstance()
+                val date: ArrayList<String> = note.date
+                    .split(".") as ArrayList<String>
+                val dd: String = date[0]
+                val month: String = date[1]
+                val year: String = date[2]
+                calendar[Calendar.DAY_OF_MONTH] = dd.toInt()
+                calendar[Calendar.MONTH] = month.toInt() - 1
+                calendar[Calendar.YEAR] = year.toInt()
+                val k = calendar.timeInMillis / 86400000
+                if (k !in set) {
+                    set.add(k)
+                    binding.calendarView.addEvent(
+                        Event(
+                            R.color.black,
+                            calendar.timeInMillis
+                        )
+                    )
+                }
+
+            }
+        }
+    }
+
+    private fun showCalendarItemRecyclerView(date: String) {
+        val listNote = viewModel.calendarSearch(date)
+        listNote.observe(viewLifecycleOwner) { list ->
+            val sortedList = list.sortedBy { allNotes ->
+                allNotes.time
+            }
+            adapter.updateList(sortedList)
         }
     }
 }
